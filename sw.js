3a6874638df6c82e8f94ac58abce30f01f883b76
Luan Gerber Siiss <luangerber666@gmail.com>
2025-09-10 19:50:30 -0300

    trabalho finalizado

---
+++ sw.js
@@ -0,0 +1,28 @@
+const CACHE_NAME = "pwa-chace-v1";
+const FILES_TO_CACHE = [
+    "/",
+    "/index.html",
+    "/manifest.json",
+    "/styles.css",
+    "/assets/midias/logo-rockstar.png",
+    "/assets/icons/logo_192x192.png",
+    "/assets/icons/logo_512x512.png"
+];
+
+// Instala o service worker
+self.addEventListener("install", (event) => {
+    event.waitUntil(
+        caches.open(CACHE_NAME).then(cache => {
+            return cache.addAll(FILES_TO_CACHE);
+        })
+    );
+});
+
+// Intercepta requisições
+self.addEventListener("fetch", (event) => {
+    event.respondWith(
+        caches.match(event.request).then(response => {
+            return response || fetch(event.request);
+        })
+    );
+});
